e5b39d5cc1bf1b338c81891a6435f6585a80bb9e
Andre3Vinicius <138264069+Andre3Vinicius@users.noreply.github.com>
2024-09-25 15:34:11 -0300

    first commit

---
+++ ifElse.py
@@ -0,0 +1,12 @@
+velocidade = 100
+
+
+
+if (velocidade > 110):
+    print('Acima da velocidade permitida')
+    print('Favor reduzir a sua velocidade')
+elif(velocidade < 55):
+    print('Por favor dirigir a cima de 60')
+
+else:
+    print('Você está na velocidade permitida')
---
+++ operadoresLogicos.py
@@ -0,0 +1,16 @@
+renda_acima_5mil = True
+nome_limpo = True
+
+if renda_acima_5mil and nome_limpo:
+    print("Financiamento aprovado")
+else:
+    print("Financiamento reprovado")
+
+
+renda_acima_6mil = True
+nome_limpo2 = False
+
+if renda_acima_6mil or nome_limpo2:
+    print("Financiamento aprovado")
+else:
+    print("Financiamento reprovado")
