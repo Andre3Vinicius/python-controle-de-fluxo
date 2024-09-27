33e0e7b65f063622887dc09bbf0185a7b76682ba
Andre3Vinicius <138264069+Andre3Vinicius@users.noreply.github.com>
2024-09-27 15:11:08 -0300

    Operadores de comparação e alguns ForLoops

---
+++ ForLoop.py
@@ -0,0 +1,17 @@
+'''
+# ForLoop para numeros
+
+for numero in range(1, 20, 2):
+    print(numero)
+    
+
+    # ForLoop para Strings
+
+for letra in 'Google':
+    print(letra)
+
+palavra = 'Google Sheets'
+
+for letra2 in palavra:
+    print(letra2)
+    '''
---
+++ OperadoresComparação.py
@@ -0,0 +1,15 @@
+
+valor = 10
+
+if valor >= 20 and valor < 40:
+    print('Produto foi aceito')
+else:
+    print("produto não aceito") 
+
+    #mesmo algoritimo, só que com o codigo mais limpo
+
+valor2 = 10
+if 20 <= valor2 < 40:
+    print('Produto foi aceito')
+else:
+    print('Produto não aceito')
